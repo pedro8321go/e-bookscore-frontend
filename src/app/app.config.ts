00c62a6a6e8645b88806ba4ea5ff26887629f7f9
Pedro Garcia <pedro8321go@gmail.com>
2024-07-25 13:14:25 -0400

    provide httpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
-import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {HttpClient, provideHttpClient} from "@angular/common/http";
+import {provideHttpClient} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
