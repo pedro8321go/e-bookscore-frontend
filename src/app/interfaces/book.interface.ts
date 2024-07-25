1928a5087c66de3f417101e4d8325b720d5444e2
Pedro Garcia <pedro8321go@gmail.com>
2024-07-25 11:35:08 -0400

    create book interface

---
+++ src/app/interfaces/book.interface.ts
@@ -0,0 +1,8 @@
+export interface Book {
+    bookId:            number;
+    title:             string;
+    authors:           string[];
+    editorial:         string;
+    short_description: string;
+    image_url:         string;
+}
