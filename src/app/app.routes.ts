33e39cd6ca77d6d92c7277092e1223eb40562d1e
Pedro Garcia <pedro8321go@gmail.com>
2024-07-25 06:17:51 -0400

    creating route with lazyload strategy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import {LayoutComponent} from "./layout/layout.component";
+import {NotFoundComponent} from "./components/pages/not-found/not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./components/pages/welcome/welcome.component').then(c =>c.WelcomeComponent),
+        title: 'E-BookScore | Welcome'
+      },
+      {
+        path: 'books',
+        loadComponent: () => import('./components/pages/books/books.component').then(c =>c.BooksComponent),
+        title: 'E-BookScore | Books'
+      },
+      {
+        path: 'books/:bookId',
+        loadComponent: () => import('./components/pages/book-details/book-details.component').then(c =>c.BookDetailsComponent),
+        title: 'E-BookScore | Book Resume'
+      }
+    ]
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+    title: 'Error 404'
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
