6d0047dcff045d0055472430d031d0fb68dd7164
Pedro Garcia <pedro8321go@gmail.com>
2024-07-25 13:16:54 -0400

    implement function for get all books

--- src/app/components/pages/books/books.component.ts
+++ src/app/components/pages/books/books.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {BookService} from "../../../services/book.service";
+import {Book} from "../../../interfaces/book.interface";
+import {JsonPipe} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [],
+  imports: [
+    JsonPipe,
+    RouterLink
+  ],
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit{
+
+  public bookService = inject(BookService);
+  data!: Book[];
+
+  ngOnInit(){
+    this.getAllBooks();
+  }
+
+  getAllBooks() {
+    this.bookService.getAllBooks().subscribe( resp =>{
+      this.data = resp;
+    })
+  }
 
 }
