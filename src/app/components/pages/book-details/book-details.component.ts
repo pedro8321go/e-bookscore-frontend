9214fe8f1b0cda6a873110f880af83293f5376e0
Pedro Garcia <pedro8321go@gmail.com>
2024-07-25 15:51:34 -0400

    implement book detail page

--- src/app/components/pages/book-details/book-details.component.ts
+++ src/app/components/pages/book-details/book-details.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {BookService} from "../../../services/book.service";
+import {Book} from "../../../interfaces/book.interface";
+import {switchMap} from "rxjs";
+import {JsonPipe, NgIf} from "@angular/common";
+
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [],
+  imports: [
+    JsonPipe,
+    NgIf
+  ],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.css'
 })
-export class BookDetailsComponent {
+export class BookDetailsComponent implements OnInit{
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private bookService = inject(BookService);
+  book!: Book | undefined;
+  ngOnInit() {
+    this.route.params.pipe(
+      switchMap(({bookId}) => this.bookService.getBookById(bookId)))
+      .subscribe(resp => {
+        this.book = resp;
+      })
 
+  }
+
+  returnEvent(){
+    this.router.navigate(['/books'])
+  }
 }
