0e40d39532ce50fdd04dd668dc18f3a16ed55283
Pedro Garcia <pedro8321go@gmail.com>
2024-07-25 09:51:54 -0400

    fixing flowbite settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {initFlowbite} from "flowbite";
+
 
 @Component({
   selector: 'app-root',
